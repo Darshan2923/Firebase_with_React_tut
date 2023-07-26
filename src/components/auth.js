73d3989bf50a90635c171d3a0c7d0f104347b66b
Darshan <darshanpatel2923@gmail.com>
2023-07-26 22:35:19 +0530

    Adding logOut in firebase

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import { auth, gprovider } from '../config/firebase-config'
-import { createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithPopup, signOut } from 'firebase/auth'
 
 function Auth() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
 
     console.log(auth?.currentUser?.email)
+    console.log(auth?.currentUser?.photoURL)
     // ? mark for existing baar baar rerender naa ho nai toh console me error aayega
     const signIn = async () => {
         try {
@@ -24,6 +25,14 @@ function Auth() {
             console.log(error);
         }
     };
+    const logout = async () => {
+        try {
+
+            await signOut(auth);
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <section id="authentication">
             <div className="auth-container">
@@ -35,6 +44,7 @@ function Auth() {
                     onChange={(e) => setPassword(e.target.value)} />
                 <button onClick={signIn}>Sign In</button>
                 <button onClick={signInwithGoogle}>SignIn With Google</button>
+                <button onClick={logout}>Log Out</button>
             </div>
         </section>
     )
