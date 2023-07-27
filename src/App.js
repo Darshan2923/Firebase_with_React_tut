fb176823ddaa567e92a89261540d463a747647ac
Darshan <darshanpatel2923@gmail.com>
2023-07-27 11:45:34 +0530

    Did Add and display  in DB

--- src/App.js
+++ src/App.js
@@ -1,15 +1,96 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Auth from './components/auth';
 // import Register from './components/Register';
+import { db } from './config/firebase-config';
+import { getDocs, collection, addDoc } from 'firebase/firestore';
 
 function App() {
+  const [movieList, setMovieList] = useState([])
+
+  const moviesCollectionRef = collection(db, "movies");
+  //Read and show from db to screen
+  //This will give an error cause we'll need to set  rules 
+
+  useEffect(() => {
+    const getMovieList = async () => {
+      //READ DATA FROM DB
+      try {
+        const data = await getDocs(moviesCollectionRef);
+        const filteredData = data.docs.map((doc) => {
+          return ({ ...doc.data(), id: doc.id });
+        })
+        //SHOW THE MOVIE LIST
+        setMovieList(filteredData);
+
+        //console.log(filteredData); //gets you your filtered Data
+        //console.log(data); //gets you an object of all the confucing stuff related to data to asked for
+      } catch (e) {
+        console.log(e);
+      }
+
+    };
+    getMovieList();
+  }, []);
+
+  //Adding new Movies into db and screen
+  const [newMovie, setNewMovie] = useState("")
+  const [newRelease, setNewRelease] = useState(0)
+  const [isHit, setIsHit] = useState(false)
+
+  const onSubmitMovie = async () => {
+    try {
+
+      await addDoc(moviesCollectionRef, {
+        name: newMovie,
+        release: newRelease,
+        hit: isHit
+      })
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  //Problem arises when we want to see on the screen but it will only display in db so for that we'll do one thing in useEffect...
+
+
   return (
     <div className="appContainer">
       <h1>Firebase course</h1>
+
+      {/* Will be configuring Firebase DB operations over here */}
+
+
+
       {/* So i gone to firebase website and do the registration and all things done there
       and then hit npm install firebase over here
       , copy paste cli and setup auth  */}
       <Auth />
+
+      {/* Setnew movies      */}
+      <div style={{ marginTop: "90px" }}>
+        <input type="text" placeholder='Movie title...'
+          onChange={(e) => setNewMovie(e.target.value)} />
+        <input type="number" placeholder='Movie release year...'
+          onChange={(e) => setNewRelease(Number(e.target.value))} />
+        <input type="checkbox" className='hit'
+          checked={isHit}
+          onChange={(e) => setIsHit(e.target.checked)} />
+        <label htmlFor="hit">Hit or not</label><br />
+        <button onClick={onSubmitMovie}>Submit movie</button>
+      </div>
+
+
+      {/* Show original movies on screen */}
+      <div>
+        {movieList.map((movie) => {
+          return (
+            <div>
+              <h1 style={{ color: movie.hit ? "green" : "red" }}>{movie.name}</h1>
+              <p>Date:{movie.release}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
